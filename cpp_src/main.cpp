d372bd53b380660edb3220d54e1ee9392886d3a1
Jonas Platte <mail@jonasplatte.de>
2014-05-06 17:04:44 +0200

    Added game subpackage, some restructuring, updated license notice

--- cpp_src/main.cpp
+++ cpp_src/main.cpp
@@ -1,15 +1,15 @@
 /* Copyright 2014 Jonas Platte
- * 
- * This file is part of the Cyvasse Online Webapp.
- * 
- * The Cyvasse Online Webapp is free software: you can redistribute it and/or modify it under the
+ *
+ * This file is part of Cyvasse Online.
+ *
+ * Cyvasse Online is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Affero General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- * 
- * The Cyvasse Online Webapp is distributed in the hope that it will be useful, but WITHOUT
+ *
+ * Cyvasse Online is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,7 +26,7 @@ int main()
 		tnt::Tntnet app;
 		//tnt::TntConfig& config = tnt::TntConfig::it();
 
-        // TODO: read webapp-conf.json
+		// TODO: read webapp-conf.json
 
 		app.listen(2517);
 		app.setAppName("cyvasse-online");
