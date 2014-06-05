d3cb8d61a50605f82f440a51eeda7f696ef2b8c6
Jonas Platte <mail@jonasplatte.de>
2014-06-05 21:08:35 +0200

    Changed URL mapping to use new (shorter) match IDs

--- src/main.cpp
+++ src/main.cpp
@@ -41,9 +41,9 @@ int main()
 		app.mapUrl("^/(.+)$", "static@tntnet").setPathInfo("$1");
 
 		// dynamic content
-		app.mapUrl("^/$",              "page").setArg("content", "index");
-		app.mapUrl("^/match/create",   "page").setArg("content", "create");
-		app.mapUrl("^/match/(.{10})$", "page").setArg("content", "game");
+		app.mapUrl("^/$",             "page").setArg("content", "index");
+		app.mapUrl("^/match/create",  "page").setArg("content", "create");
+		app.mapUrl("^/match/(.{4})$", "page").setArg("content", "game");
 
 		app.run();
 	}
