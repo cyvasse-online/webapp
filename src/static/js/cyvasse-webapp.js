ccc5117ab3a72d038546804d1364e54183bac028
Jonas Platte <mail@jonasplatte.de>
2014-06-08 15:16:29 +0200

    Added player color selection, some small bugfixes

--- src/static/js/cyvasse-webapp.js
+++ src/static/js/cyvasse-webapp.js
@@ -1,13 +1,19 @@
 function setupCreateMatchHandlers()
 {
-	$('#create-match-submit-private').click(function() {
-		// is it okay security-wise to use window.location.hostname just like that?
-		var conn = new WebSocket('ws://' + window.location.hostname + ':2516/');
+	$("#create-match-submit-private").click(function() {
+		ruleSet = $("#create-match-rule-set").val();
+		color   = $("input:radio[name=color]:checked").val();
+		if(ruleSet == null || color == null)
+			return;
+
+		// is it okay security-wise to use window.location.hostname here?
+		var conn = new WebSocket("ws://" + window.location.hostname + ":2516/");
 		conn.onopen = function() {
 			var createGameRequest = {
 			  "action": "create game",
 			  "param": {
-				"ruleSet": "mikelepage"
+				"ruleSet": ruleSet,
+				"color": color
 			  }
 			};
 			conn.send(JSON.stringify(createGameRequest));
@@ -16,7 +22,8 @@ function setupCreateMatchHandlers()
 		conn.onmessage = function(msg) {
 			var response = JSON.parse(msg.data);
 			if(response.success === true) {
-				window.location.pathname = '/match/' + response.b64ID;
+				conn.close();
+				window.location.pathname = "/match/" + response.matchID;
 			}
 			// else
 		};
